2de5b20c28d06ddf53a2acfe6b6b203801b6907c
Andrew Bissell <abissell@gmail.com>
2020-05-26 03:50:55 +0000

    ch 9 structs

---
+++ ch-9/structs/src/main.rs
@@ -0,0 +1,191 @@
+#![allow(dead_code)]
+
+fn main() {
+    println!("Hello, world!");
+    let width = 1024;
+    let height = 576;
+    let image = GrayscaleMap {
+        pixels: vec![0; width * height],
+        size: (width, height),
+    };
+
+    assert_eq!(image.size, (1024, 576));
+    assert_eq!(image.pixels.len(), 1024 * 576);
+
+    let hokey = Broom {
+        name: "Hokey".to_string(),
+        height: 60,
+        health: 100,
+        position: (100.0, 200.0, 0.0),
+        intent: BroomIntent::FetchWater,
+    };
+
+    let (hokey1, hokey2) = chop(hokey);
+    assert_eq!(hokey1.name, "Hokey I");
+    assert_eq!(hokey1.health, 100);
+
+    assert_eq!(hokey2.name, "Hokey II");
+    assert_eq!(hokey2.health, 100);
+
+    let image_bounds = Bounds(1024, 768);
+    assert_eq!(image_bounds.0 * image_bounds.1, 786432);
+
+    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('=');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('='));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('I');
+    assert!(!q.is_empty());
+
+    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    let (older, younger) = q.split();
+    // q is now uninitialized.
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
+
+    let mut q = Queue::new();
+    q.push('*');
+
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
+}
+
+fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
+    assert_eq!(pixels.len(), size.0 * size.1);
+    GrayscaleMap { pixels, size }
+}
+
+/// A rectangle of eight-bit grayscale pixels.
+pub struct GrayscaleMap {
+    pub pixels: Vec<u8>,
+    pub size: (usize, usize),
+}
+
+// In this game, brooms are monsters. You'll see.
+struct Broom {
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+/// Two possible alternatives for what a `Broom` could be working on.
+#[derive(Copy, Clone)]
+enum BroomIntent { FetchWater, DumpWater }
+
+// Receive the input Broom by value, taking ownership.
+fn chop(b: Broom) -> (Broom, Broom) {
+    // Initialize `broom1` mostly from `b`, changing only `height`. Since
+    // `String` is not `Copy`, `broom1` takes ownership of `b`'s name.
+    let mut broom1 = Broom { height: b.height / 2, .. b};
+
+    // Initialize `broom2` mostly from `broom1`. Since `String` is not
+    // `Copy`, we must clone `name` explicitly.
+    let mut broom2 = Broom { name: broom1.name.clone(), .. broom1 };
+
+    // Give each fragment a distinct name.
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
+}
+
+struct Bounds(usize, usize);
+
+/// A first-in, first-out queue of characters.
+pub struct Queue<T> {
+    older: Vec<T>, // older elements, eldest last.
+    younger: Vec<T>, // younger elements, youngest last.
+}
+
+impl<T> Queue<T> {
+    /// Push a character onto the back of a queue.
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
+    }
+
+    /// Pop a character off the front of a queue. Return `Some(c)` if there
+    /// was a character to pop, or `None` if the queue was empty.
+    pub fn pop(&mut self) -> Option<T> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            // Bring the elements in younger over to older, and put them in
+            // the promised order.
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        // Now older is guaranteed to have something. Vec's pop method
+        // already returns an Option, so we're set.
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
+        (self.older, self.younger)
+    }
+
+    pub fn new() -> Queue<T> {
+        Queue { older: Vec::new(), younger: Vec::new() }
+    }
+}
+
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32,
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for i in 1..slice.len() {
+        if slice[i] < *least { least = &slice[i]; }
+        if slice[i] > *greatest { greatest = &slice[i]; }
+    }
+    Extrema { greatest, least }
+}
+
+use std::cell::Cell;
+
+pub struct SpiderRobot {
+    species: String,
+    web_enabled: bool,
+    hardware_error_count: Cell<u32>,
+}
+
+impl SpiderRobot {
+    /// Increase the error count by 1.
+    pub fn add_hardware_error(&self) {
+        let n = self.hardware_error_count.get();
+        self.hardware_error_count.set(n + 1);
+    }
+
+    /// True if any hardware errors have been reported.
+    pub fn has_hardware_errors(&self) -> bool {
+        self.hardware_error_count.get() > 0
+    }
+}
